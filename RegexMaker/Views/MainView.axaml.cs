56ce23c2995d588290c5670d0788334413e24097
Darrell Plank <jar_czar@msn.com>
2026-02-22 00:35:36 -0800

    Some little comment stuff

--- RegexMaker/Views/MainView.axaml.cs
+++ RegexMaker/Views/MainView.axaml.cs
@@ -7,6 +7,10 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+
+        // Exemplars are set up in the static constructor of RgxNode using reflection to find all derived types
+        // from RgxNode and create instances of them. We can just loop through those exemplars and create buttons
+        // for each of them in the UI.
         foreach (var RgxNode in Nodes.RgxNode.Exemplars)
         {
             var btn = new Button
