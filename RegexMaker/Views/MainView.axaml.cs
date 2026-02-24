1904c9cb47fe89764877fb8092334e7e967cd68a
Darrell Plank <jar_czar@msn.com>
2026-02-24 13:58:45 -0800

    Start of parameters pane - put in carousel and arranged to have controls for each RgxNodeType.

--- RegexMaker/Views/MainView.axaml.cs
+++ RegexMaker/Views/MainView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Layout;
+using RegexMaker.Nodes;
 
 namespace RegexMaker.Views;
 
@@ -31,6 +32,71 @@ public partial class MainView : UserControl
             SpToolBox.Children.Add(text);
             text.IsVisible = true;
         }
+    }
 
+    /// <summary>
+    /// Shows the parameter page in the carousel corresponding to the given RgxNodeType
+    /// </summary>
+    internal void ShowNodeParameters(RgxNodeType nodeType)
+    {
+        CarParameters.SelectedIndex = (int)nodeType;
+    }
+
+    /// <summary>
+    /// Gets parameter values from the carousel for StringSearch node
+    /// </summary>
+    public string GetStringSearchValue()
+    {
+        return TxtStringSearchValue.Text ?? string.Empty;
+    }
+
+    /// <summary>
+    /// Sets parameter values in the carousel for StringSearch node
+    /// </summary>
+    public void SetStringSearchValue(string value)
+    {
+        TxtStringSearchValue.Text = value;
+    }
+
+    /// <summary>
+    /// Gets parameter values from the carousel for Repeat node
+    /// </summary>
+    public (int Least, int Most, bool IsLazy) GetRepeatValues()
+    {
+        return (
+            (int)(NumRepeatLeast.Value ?? 0),
+            (int)(NumRepeatMost.Value ?? -1),
+            ChkRepeatIsLazy.IsChecked ?? false
+        );
+    }
+
+    /// <summary>
+    /// Sets parameter values in the carousel for Repeat node
+    /// </summary>
+    public void SetRepeatValues(int least, int most, bool isLazy)
+    {
+        NumRepeatLeast.Value = least;
+        NumRepeatMost.Value = most;
+        ChkRepeatIsLazy.IsChecked = isLazy;
+    }
+
+    /// <summary>
+    /// Gets parameter values from the carousel for Range node
+    /// </summary>
+    public (string CharStart, string CharEnd) GetRangeValues()
+    {
+        return (
+            TxtRangeCharStart.Text ?? "a",
+            TxtRangeCharEnd.Text ?? "z"
+        );
+    }
+
+    /// <summary>
+    /// Sets parameter values in the carousel for Range node
+    /// </summary>
+    public void SetRangeValues(string charStart, string charEnd)
+    {
+        TxtRangeCharStart.Text = charStart;
+        TxtRangeCharEnd.Text = charEnd;
     }
 }
